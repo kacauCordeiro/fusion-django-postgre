262af533907d53399287e6b46fac8d52f89b603f
kauane <kauane@maisresultado.com>
2021-05-20 17:00:48 -0300

    ajuste

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0b+q*-@p-tm^pcv)glpfu(l^!h)h3vwspq7((*(4%@ug7cl_pl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
